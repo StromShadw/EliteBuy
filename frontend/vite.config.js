18f88a2f3bf84b7028206117479129619a4ec04b
Paresh Esave <esaveparesh@gmail.com>
2024-11-30 22:05:36 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,36 +1,18 @@
-// // vite.config.js
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://elitebuy-1.onrender.com',
-//         changeOrigin: true,
-//       },
-//       '/user': {
-//         target: 'https://elitebuy-1.onrender.com',
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-  
-// });
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://elitebuy-1.onrender.com',
+      "/api": {
+        target: process.env.VITE_API_URL || "https://elitebuy.onrender.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'), // Ensure '/api' prefix is preserved
       },
-      '/user': {
-        target: 'https://elitebuy-1.onrender.com',
+      "/user": {
+        target: process.env.VITE_API_URL || "https://elitebuy.onrender.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/user/, '/user'), // Same as above
       },
     },
   },
